deeb989f3a1bd2521bdeb6ac5bd966ba0b35a122
MarcSM <discotazo@gmail.com>
2020-06-25 00:22:39 +0200

    CN

--- Source/Panels/TopPanel.h
+++ Source/Panels/TopPanel.h
@@ -1,11 +1,19 @@
-/*
- ==============================================================================
- 
- TopPanel.h
- Created: 12 May 2020 11:18:41am
- Author:  Marc Sanchez Martinez
- 
- ==============================================================================
+/* Copyright (C) 2020 Marc Sanchez Martinez
+ *
+ * https://github.com/MarcSM/morphex
+ *
+ * Morphex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Morphex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Morphex. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
